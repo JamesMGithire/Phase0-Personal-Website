28a3ae01290e78a820adaf637aaac61bf3861ad6
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-20 20:27:32 +0300

    nomadBar Complete

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let rect0 = aElements[0].getBoundingClientRect()
 nomad.style.left = `${rect0.left}px`;
 nomad.style.width = `${rect0.width - 25}px`
 // https://usefulangle.com/post/113/javascript-detecting-element-visible-during-scroll
-window.addEventListener('scroll',()=>{ 
+window.addEventListener('scroll', () => {
     nomadFun();
-    nomad.style.transition = "all 3s";
-    nomad.style.left= (nomadFun());
+    nomad.style.transition = "all 0.7s";
+    nomad.style.left = (nomadFun());
 });
 
-function nomadFun () {
+function nomadFun() {
     // let nomad = document.getElementById('nomadHr');
     let element0 = document.querySelector('#page-top')
         .getBoundingClientRect();
@@ -26,30 +26,16 @@ function nomadFun () {
     let secDivs = this.document.getElementsByClassName("section");
     try {
         for (el in secDivs) {
-            if (secDivs[el].getBoundingClientRect().y <= 82 && secDivs[el].getBoundingClientRect().y >= -600) {
+            if (secDivs[el].getBoundingClientRect().y <= (window.innerHeight/2) && secDivs[el].getBoundingClientRect().y >= -(secDivs[el].getBoundingClientRect().height-200)) {
                 nomad.style.width = `${aElements[el].getBoundingClientRect().width - 20}px`
-                console.log('Element '+el +' is fully visible in screen at ' + secDivs[el].getBoundingClientRect().y);
+                console.log('Element ' + el + ' is fully visible in screen at ' + secDivs[el].getBoundingClientRect().y);
 
                 return `${aElements[el].getBoundingClientRect().left}px`;
             }
-            console.log(secDivs[el].getBoundingClientRect().y);
+            console.log(secDivs[el].getBoundingClientRect().height);
         }
     }
     catch (err) {
         // Do nothing... Good boy.
     }
-    // switch (true) {
-    //     case element1.y <= 230 && element1.y >= -500:
-    //         nomad.style.left = `${rect1.left}px`;
-    //         nomad.style.width = `${rect1.width - 20}px`
-    //         console.log('Element 1 is fully visible in screen at ' + element1.y);
-    //         break;
-    //     case element0.y <= 230 && element0.y >= -500:
-    //         nomad.style.left = `${rect0.left}px`;
-    //         nomad.style.width = `${rect0.width - 20}px`
-    //         console.log('Element 0 is fully visible in screen at ' + element0.y);
-    //         break;
-
-    // }
-
 }
